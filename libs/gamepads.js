a78b759bb6c65950b9f5aeb5b6ae8b1db066a9f9
JoeCoding3 <tiredredstoner@gmail.com>
2025-10-25 01:09:09 -0700

    Gamepad usability improvement

--- libs/gamepads.js
+++ libs/gamepads.js
@@ -63,6 +63,7 @@ class SimpleGamepad {
 }
 class SimpleGamepadCursor {
     static repeatDelay = 500
+    static reRepeatDelay = 50
     static deadzone = 0.6
 
     #gamepad
@@ -75,21 +76,21 @@ class SimpleGamepadCursor {
         let timestamp = this.#gamepad.timestamp
         if (this.#leftInit == false) this.#leftInit = joystick[0] >= -SimpleGamepadCursor.deadzone
         else if (this.#leftTimestamp == -1) this.#leftTimestamp = joystick[0] < -SimpleGamepadCursor.deadzone ? timestamp : -1
-        else if (joystick[0] >= -SimpleGamepadCursor.deadzone) this.#leftTimestamp = -1, this.#hasStartedLeft = false
+        else if (joystick[0] >= -SimpleGamepadCursor.deadzone) this.#leftTimestamp = -1, this.#leftRepeatTimestamp = -1, this.#hasStartedLeft = false
         if (this.#rightInit == false) this.#rightInit = joystick[0] <= SimpleGamepadCursor.deadzone
         else if (this.#rightTimestamp == -1) this.#rightTimestamp = joystick[0] > SimpleGamepadCursor.deadzone ? timestamp : -1
-        else if (joystick[0] <= SimpleGamepadCursor.deadzone) this.#rightTimestamp = -1, this.#hasStartedRight = false
+        else if (joystick[0] <= SimpleGamepadCursor.deadzone) this.#rightTimestamp = -1, this.#rightRepeatTimestamp = -1, this.#hasStartedRight = false
         if (this.#upInit == false) this.#upInit = joystick[1] >= -SimpleGamepadCursor.deadzone
         else if (this.#upTimestamp == -1) this.#upTimestamp = joystick[1] < -SimpleGamepadCursor.deadzone ? timestamp : -1
-        else if (joystick[1] >= -SimpleGamepadCursor.deadzone) this.#upTimestamp = -1, this.#hasStartedUp = false
+        else if (joystick[1] >= -SimpleGamepadCursor.deadzone) this.#upTimestamp = -1, this.#upRepeatTimestamp = -1, this.#hasStartedUp = false
         if (this.#downInit == false) this.#downInit = joystick[1] <= SimpleGamepadCursor.deadzone
         else if (this.#downTimestamp == -1) this.#downTimestamp = joystick[1] > SimpleGamepadCursor.deadzone ? timestamp : -1
-        else if (joystick[1] <= SimpleGamepadCursor.deadzone) this.#downTimestamp = -1, this.#hasStartedDown = false
+        else if (joystick[1] <= SimpleGamepadCursor.deadzone) this.#downTimestamp = -1, this.#downRepeatTimestamp = -1, this.#hasStartedDown = false
 
         let isClicking = this.#gamepad.buttons.findIndex(val => val > 0) > -1
         if (this.#clickInit == false) this.#clickInit = !isClicking
         else if (this.#clickTimestamp == -1) this.#clickTimestamp = isClicking ? timestamp : -1
-        else if (!isClicking) this.#clickTimestamp = -1, this.#hasStartedClick = false
+        else if (!isClicking) this.#clickTimestamp = -1, this.#clickRepeatTimestamp = -1, this.#hasStartedClick = false
     }
 
     #leftTimestamp = -1
@@ -97,6 +98,11 @@ class SimpleGamepadCursor {
     #upTimestamp = -1
     #downTimestamp = -1
     #clickTimestamp = -1
+    #leftRepeatTimestamp = -1
+    #rightRepeatTimestamp = -1
+    #upRepeatTimestamp = -1
+    #downRepeatTimestamp = -1
+    #clickRepeatTimestamp = -1
     #hasStartedLeft = false
     #hasStartedRight = false
     #hasStartedUp = false
@@ -108,33 +114,83 @@ class SimpleGamepadCursor {
     #downInit = false
     #clickInit = false
     get shouldMoveLeft () {
+        let timestamp = this.#gamepad.timestamp
         if (!this.#leftInit) return false
-        else if (this.#hasStartedLeft) return this.#gamepad.timestamp - this.#leftTimestamp >= SimpleGamepadCursor.repeatDelay
-        else if (this.#leftTimestamp > -1) return this.#hasStartedLeft = true
+        else if (this.#hasStartedLeft) {
+            if (this.#leftRepeatTimestamp == -1) {
+                let val = timestamp - this.#leftTimestamp >= SimpleGamepadCursor.repeatDelay
+                if (val) this.#leftRepeatTimestamp = timestamp
+                return val
+            } else {
+                let val = timestamp - this.#leftRepeatTimestamp >= SimpleGamepadCursor.reRepeatDelay
+                if (val) this.#leftRepeatTimestamp = timestamp
+                return val
+            }
+        } else if (this.#leftTimestamp > -1) return this.#hasStartedLeft = true
         else return false
     }
     get shouldMoveRight () {
+        let timestamp = this.#gamepad.timestamp
         if (!this.#rightInit) return false
-        else if (this.#hasStartedRight) return this.#gamepad.timestamp - this.#rightTimestamp >= SimpleGamepadCursor.repeatDelay
-        else if (this.#rightTimestamp > -1) return this.#hasStartedRight = true
+        else if (this.#hasStartedRight) {
+            if (this.#rightRepeatTimestamp == -1) {
+                let val = timestamp - this.#rightTimestamp >= SimpleGamepadCursor.repeatDelay
+                if (val) this.#rightRepeatTimestamp = timestamp
+                return val
+            } else {
+                let val = timestamp - this.#rightRepeatTimestamp >= SimpleGamepadCursor.reRepeatDelay
+                if (val) this.#rightRepeatTimestamp = timestamp
+                return val
+            }
+        } else if (this.#rightTimestamp > -1) return this.#hasStartedRight = true
         else return false
     }
     get shouldMoveUp () {
+        let timestamp = this.#gamepad.timestamp
         if (!this.#upInit) return false
-        else if (this.#hasStartedUp) return this.#gamepad.timestamp - this.#upTimestamp >= SimpleGamepadCursor.repeatDelay
-        else if (this.#upTimestamp > -1) return this.#hasStartedUp = true
+        else if (this.#hasStartedUp) {
+            if (this.#upRepeatTimestamp == -1) {
+                let val = timestamp - this.#upTimestamp >= SimpleGamepadCursor.repeatDelay
+                if (val) this.#upRepeatTimestamp = timestamp
+                return val
+            } else {
+                let val = timestamp - this.#upRepeatTimestamp >= SimpleGamepadCursor.reRepeatDelay
+                if (val) this.#upRepeatTimestamp = timestamp
+                return val
+            }
+        } else if (this.#upTimestamp > -1) return this.#hasStartedUp = true
         else return false
     }
     get shouldMoveDown () {
+        let timestamp = this.#gamepad.timestamp
         if (!this.#downInit) return false
-        else if (this.#hasStartedDown) return this.#gamepad.timestamp - this.#downTimestamp >= SimpleGamepadCursor.repeatDelay
-        else if (this.#downTimestamp > -1) return this.#hasStartedDown = true
+        else if (this.#hasStartedDown) {
+            if (this.#downRepeatTimestamp == -1) {
+                let val = timestamp - this.#downTimestamp >= SimpleGamepadCursor.repeatDelay
+                if (val) this.#downRepeatTimestamp = timestamp
+                return val
+            } else {
+                let val = timestamp - this.#downRepeatTimestamp >= SimpleGamepadCursor.reRepeatDelay
+                if (val) this.#downRepeatTimestamp = timestamp
+                return val
+            }
+        } else if (this.#downTimestamp > -1) return this.#hasStartedDown = true
         else return false
     }
     get shouldClick () {
+        let timestamp = this.#gamepad.timestamp
         if (!this.#clickInit) return false
-        else if (this.#hasStartedClick) return this.#gamepad.timestamp - this.#clickTimestamp >= SimpleGamepadCursor.repeatDelay
-        else if (this.#clickTimestamp > -1) return this.#hasStartedClick = true
+        else if (this.#hasStartedClick) {
+            if (this.#clickRepeatTimestamp == -1) {
+                let val = timestamp - this.#clickTimestamp >= SimpleGamepadCursor.repeatDelay
+                if (val) this.#clickRepeatTimestamp = timestamp
+                return val
+            } else {
+                let val = timestamp - this.#clickRepeatTimestamp >= SimpleGamepadCursor.reRepeatDelay
+                if (val) this.#clickRepeatTimestamp = timestamp
+                return val
+            }
+        } else if (this.#clickTimestamp > -1) return this.#hasStartedClick = true
         else return false
     }
 }
