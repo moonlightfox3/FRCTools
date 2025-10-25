8e1c6ba9d4918b2d97840750faaaf2c639b02bd7
JoeCoding3 <tiredredstoner@gmail.com>
2025-10-25 01:26:14 -0700

    Gamepad fix

--- libs/indexCursor.js
+++ libs/indexCursor.js
@@ -23,26 +23,30 @@ function gamepadUpdate () {
         indexCursorUnhighlight(gamepadCursorEls[gamepadCursorPos])
         gamepadSeen = false
     } else {
-        gamepad.cursor.update()
-        
-        if (gamepadSeen) {
-            let oldCursorPos = gamepadCursorPos
-            if (gamepadCursorIsHorizontal) {
-                if (gamepad.cursor.shouldMoveLeft && gamepadCursorPos > 0) gamepadCursorPos--
-                if (gamepad.cursor.shouldMoveRight && gamepadCursorPos < gamepadCursorEls.length - 1) gamepadCursorPos++
-            } else {
-                if (gamepad.cursor.shouldMoveUp && gamepadCursorPos > 0) gamepadCursorPos--
-                if (gamepad.cursor.shouldMoveDown && gamepadCursorPos < gamepadCursorEls.length - 1) gamepadCursorPos++
-            }
+        try { // 'try' is only needed here because some browsers don't reset the 'gamepad' variable for some reason
+            gamepad.cursor.update()
+            
+            if (gamepadSeen) {
+                let oldCursorPos = gamepadCursorPos
+                if (gamepadCursorIsHorizontal) {
+                    if (gamepad.cursor.shouldMoveLeft && gamepadCursorPos > 0) gamepadCursorPos--
+                    if (gamepad.cursor.shouldMoveRight && gamepadCursorPos < gamepadCursorEls.length - 1) gamepadCursorPos++
+                } else {
+                    if (gamepad.cursor.shouldMoveUp && gamepadCursorPos > 0) gamepadCursorPos--
+                    if (gamepad.cursor.shouldMoveDown && gamepadCursorPos < gamepadCursorEls.length - 1) gamepadCursorPos++
+                }
 
-            if (gamepadCursorPos != oldCursorPos) {
-                indexCursorUnhighlight(gamepadCursorEls[oldCursorPos])
-                indexCursorHighlight(gamepadCursorEls[gamepadCursorPos])
-            }
-        } else indexCursorHighlight(gamepadCursorEls[gamepadCursorPos])
-        gamepadSeen = true
+                if (gamepadCursorPos != oldCursorPos) {
+                    indexCursorUnhighlight(gamepadCursorEls[oldCursorPos])
+                    indexCursorHighlight(gamepadCursorEls[gamepadCursorPos])
+                }
+            } else indexCursorHighlight(gamepadCursorEls[gamepadCursorPos])
+            gamepadSeen = true
 
-        if (gamepad.cursor.shouldClick) indexCursorClick(gamepadCursorEls[gamepadCursorPos])
+            if (gamepad.cursor.shouldClick) indexCursorClick(gamepadCursorEls[gamepadCursorPos])
+        } catch (er) {
+            gamepad = null
+        }
     }
     
     requestAnimationFrame(gamepadUpdate)
