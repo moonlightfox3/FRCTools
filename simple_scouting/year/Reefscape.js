ed25cf4c01a07011d51d704cf3a6ca5879b4c1be
JoeCoding3 <tiredredstoner@gmail.com>
2025-10-28 17:30:23 -0700

    Download fix

--- simple_scouting/year/Reefscape.js
+++ simple_scouting/year/Reefscape.js
@@ -250,7 +250,7 @@ function modifyInputValueGamepad (inputTeleopHit, inputTeleopMiss, inputAutoHit,
         else inputAutoHit.value = parseInt(inputAutoHit.value) + val
     }
 }
-saveData.onclick = () => downloadData()
+saveData.onclick = () => downloadData();
 
 onbeforeunload = function (ev) {
     let formChanged =
