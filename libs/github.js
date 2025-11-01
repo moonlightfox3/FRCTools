5ca6efcf109e3a9aa12cc300cf2cdc42db024745
JoeCoding3 <tiredredstoner@gmail.com>
2025-10-31 21:43:17 -0700

    GitHub integration to show the current version

--- libs/github.js
+++ libs/github.js
@@ -6,10 +6,16 @@ async function getCommit () {
     return json[0]
 }
 let commit = null
-(async function () {
+;(async function () {
     commit = await getCommit()
+
+    commitId = getCommitId()
+    commitDate = getCommitDate()
+    showCommitUpdate()
 })()
 
+let commitId = null
+let commitDate = null
 function getCommitId () {
     // Was the commit fetched?
     if (commit == null) return null
@@ -20,7 +26,25 @@ function getCommitDate () {
     // Was the commit fetched?
     if (commit == null) return null
     
-    let date = new Date(commit.committer.date)
+    let date = new Date(commit.commit.committer.date)
     let str = date.toLocaleDateString("en-US", {hour: "numeric", minute: "numeric"})
     return str
+}
+
+let updateDateEl = null
+function showCommitUpdate () {
+    hideCommitUpdate()
+    
+    updateDateEl = document.createElement("div")
+    updateDateEl.innerText = `Updated ${commitDate} (commit ${commitId})`
+    updateDateEl.style.position = "sticky"
+    updateDateEl.style.left = "5px"
+    updateDateEl.style.top = "calc(100% - 30px)"
+    document.body.append(updateDateEl)
+}
+function hideCommitUpdate () {
+    if (updateDateEl != null) {
+        updateDateEl.remove()
+        updateDateEl = null
+    }
 }
